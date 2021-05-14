b04f2642867460bd465846b8d1106d8d975d8595
iiAlosha <arma.55@outlook.com>
2021-05-14 09:07:52 +0300

    Proje dosyası ekle.

---
+++ WeaponCamViewController/WeaponCamViewController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+namespace WeaponCamViewController
+{
+    public class WeaponCamViewController : BaseScript
+    {
+        public WeaponCamViewController()
+        {
+            EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+        }
+
+        private void OnClientResourceStart(string resourceName)
+        {
+            if (API.GetCurrentResourceName() != resourceName) return;
+
+            Tick += OnTick;
+        }
+
+
+        private Task OnTick()
+        {
+            if (LocalPlayer.Character.Weapons.Current != null &&
+                LocalPlayer.Character.Weapons.Current.Hash != WeaponHash.Unarmed &&
+                LocalPlayer.Character.Weapons.Current.AmmoType != AmmoType.Melee)
+            {
+                if (LocalPlayer.Character.CurrentVehicle != null && LocalPlayer.IsAiming && API.GetFollowVehicleCamViewMode() != 4)
+                {
+                    API.SetFollowVehicleCamViewMode(4);
+                }
+                else if(API.GetFollowPedCamViewMode() != 4)
+                {
+                    API.SetFollowPedCamViewMode(4);
+                }
+            }
+
+            return Delay(0);
+        }
+    }
+}
