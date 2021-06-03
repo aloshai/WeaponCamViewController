8abe32dd21cc689dcfe1d1159c5c1b7e6f73ef81
Mert Yılmaz <arma.55@outlook.com>
2021-06-04 00:40:32 +0300

    Delete WeaponCamViewController/output directory

--- WeaponCamViewController/output/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'Alosha'
-description 'When a player gets a gun in his hand, he forcibly changes his point of view.'
-version '1.0.0'
-
-client_script 'WeaponCamViewController.net.dll'
